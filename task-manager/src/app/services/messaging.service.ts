c801a00685fed1930b8e21d1399f34582b897b8d
BudhiChristian <cjbudhi95@gmail.com>
2020-07-20 18:45:03 -0400

    Adding method to publish to exchange

--- task-manager/src/app/services/messaging.service.ts
+++ task-manager/src/app/services/messaging.service.ts
@@ -6,14 +6,33 @@ export class MQConnection {
     private static __channel: Channel;
 
     private static readonly __connectionUrl: string = environment.messagingConfigurations.messagingUrl;
+    private static readonly __exchangeName: string = environment.messagingConfigurations.exchangeName;
 
     static async getChannel(): Promise<Channel> {
         if (!MQConnection.__channel) {
             console.log("connecting to rabbitmq");
             console.log(MQConnection.__connectionUrl);
             try {
-                MQConnection.__connection = await amqp.connect(MQConnection.__connectionUrl);
-                MQConnection.__channel = await MQConnection.__connection.createChannel();
+                // await and check again in case connection was created in a parallel thread
+                let connection = await amqp.connect(MQConnection.__connectionUrl);
+                if (!MQConnection.__connection) {
+                    MQConnection.__connection = connection;
+                } else {
+                    connection.close();
+                }
+                
+                let channel = await MQConnection.__connection.createChannel();
+                if(!MQConnection.__channel) { 
+                    MQConnection.__channel = channel;
+                } else {
+                    channel.close();
+                    connection.close();
+                }
+
+                //Assert Exchange
+                await MQConnection.__channel.assertExchange(MQConnection.__exchangeName, 'topic', {
+                    durable: true
+                })
                 console.log("rabbitmq channel established");
             } catch (err) {
                 console.error(err.message);
@@ -22,6 +41,17 @@ export class MQConnection {
             }
         }
         return MQConnection.__channel
+    }
+
+    static async publish(key: string, data: Buffer): Promise<string> {
+        let channel: Channel = await MQConnection.getChannel();
+        if (!channel) {
+            return "rabbitmq connection not established. try again.";
+        }
+
+        channel.publish(MQConnection.__exchangeName, key, data);
+
+        return "successfully queued"
     }
 
     static async publishDirectlyToQueue(queueName: string, data: Buffer): Promise<string> {
@@ -52,8 +82,6 @@ export class MQConnection {
         }
     }
 }
-// MQConnection.__channel = null;
-// MQConnection.__connection = null;
 
 process.on('exit', (code) => {
     MQConnection.close()
--- task-manager/src/environments/environment.ts
+++ task-manager/src/environments/environment.ts
@@ -5,6 +5,7 @@ export const environment = {
     },
     messagingConfigurations: {
         messagingUrl: `amqp://${process.env.RABBITMQ_HOST || '0.0.0.0'}`,
+        exchangeName: "training_exchange",
         crfTrainingQueue: "crf_training_queue"
     }
 };
